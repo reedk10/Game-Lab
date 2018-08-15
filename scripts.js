050175e879230f2815cd3182892d2b4633552ea2
reedk10 <40337728+reedk10@users.noreply.github.com>
2018-08-15 15:13:25 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,56 @@
-
-const reply = prompt("Do you like to play?");
-
-if (reply == "Yes" || reply == "yes" || reply == "YES" ) {
-    let userHealth = 40;
-    let grantHealth = 10;
-    let userName = prompt("Enter your name?");
-    let wins = 0;
-    
-    while (grantHealth > 0) {
-        let rand1 = Math.floor(Math.random() * 2 + 1);
-        let rand2 = Math.floor(Math.random() * 2 + 1);
-        userHealth -= rand1;
-        grantHealth -= rand2;
-        console.log(`${userName} has ${userHealth} health left. `);
-        console.log(`Grant the mighty chicken has ${grantHealth} health left.`);
-
-        if (grantHealth <= 0) wins += 1;
-
-        if (wins == 3  && grantHealth <= 0) {
-            console.log(`${userName} you won the fight`);
-            
-        } else if (userHealth <= 0) {
-            console.log(`Grant won!`);
-            
-        } else if (grantHealth <= 0) {
-        console.log(`${userName} wins: ${wins}`);
-            grantHealth = 10;
-        }
-    }
+function startGame() {
+  const reply = prompt("Do you want to play?");
+  if (reply == "Yes" || reply == "yes" || reply == "YES") {
+    let userName = prompt("Enter your name");
+    startCombat(userName);
+  }
 }
+
+function getDamage() {
+  return Math.floor(Math.random() * 5 + 1);
+}
+
+let a = function("attack", "quit"){
+  let game = prompt("Do you want to attack or quit?");
+   if(a == "quit")
+   console.log("game over")
+}else (a == "attack")
+return startCombat; 
+
+function startCombat(userName) {
+  let userHealth = 40;
+  let grantHealth = 10;
+  let wins = 0;
+  let game = prompt("Do you want to attack or quit?")
+
+  while (grantHealth > 0) {
+    userHealth -= getDamage();
+    grantHealth -= getDamage();
+    console.log(`${userName} has ${userHealth} health left.`);
+    console.log(`Grant the mighty chicken has ${grantHealth} health left.`);
+
+    if (grantHealth <= 0) wins += 1;
+
+    if (wins == 3 && grantHealth <= 0) {
+      console.log(`${userName} you won the fight`);
+
+    } else if (userHealth <= 0) {
+      console.log(`Grant won!`);
+
+    } else if (grantHealth <= 0) {
+      console.log(`${userName} wins: ${wins}`);
+      grantHealth = 10;
+    }
+  }
+}
+startGame();
+startCombat();
+getDamage();
+
+
+
+
+
+
+
 
